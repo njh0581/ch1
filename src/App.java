24d2fe61e898dd38d933619f843841b77eb60fcc
njh0581 <157439365+njh0581@users.noreply.github.com>
2024-05-08 12:55:43 +0900

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ public class App {
   public static void main(String[] args) throws Exception {
     System.out.println("Hello, World!");
     System.out.println("안녕하세요");
+    System.out.println("안녕하세요");
   }
 }
